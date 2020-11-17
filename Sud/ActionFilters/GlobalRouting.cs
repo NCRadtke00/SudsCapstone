f5f02d26204500250ba0613b5c48526798b11cae
Nicholas Radtke <Raddnc@gmail.com>
2020-11-16 20:18:58 -0600

    added globalrouting class

---
+++ Sud/ActionFilters/GlobalRouting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+
+namespace Sud.ActionFilters
+{
+    public class GlobalRouting : IActionFilter
+    {
+        private readonly ClaimsPrincipal _claimsPrincipal;
+        public GlobalRouting(ClaimsPrincipal claimsPrincipal)
+        {
+            _claimsPrincipal = claimsPrincipal;
+        }
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = context.RouteData.Values["controller"];
+            if (controller.Equals("Home"))
+            {
+                if (_claimsPrincipal.IsInRole("Admin"))
+                {
+                    context.Result = new RedirectToActionResult("Index", "Admin", null);
+                }
+                else if (_claimsPrincipal.IsInRole("Employee"))
+                {
+                    context.Result = new RedirectToActionResult("Index", "Employees", null);
+                }
+                else if (_claimsPrincipal.IsInRole("Customer"))
+                {
+                    context.Result = new RedirectToActionResult("Index", "Customers", null);
+                }
+            }
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
+}
