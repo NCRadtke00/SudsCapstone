8a34c1901d5cfdfc546ad2ca002e5f3c2003fc48
Nicholas Radtke <Raddnc@gmail.com>
2020-11-17 13:34:27 -0600

    created ServiceMenu, under ViewComponent

---
+++ Sud/Components/ServiceMenu.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sud.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sud.Components
+{
+    public class ServiceMenu : ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+        public ServiceMenu(ApplicationDbContext context)
+        {
+            db = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var services = await db.Services.OrderBy(c => c.Name).ToListAsync();
+            return View(services);
+        }
+    }
+}
