6915fcf8b02078585ec131c87fbbb6868df6c605
Nicholas Radtke <Raddnc@gmail.com>
2020-11-17 13:41:52 -0600

    created caroselmenu

---
+++ Sud/Components/CarouselMenu.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sud.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sud.Components
+{
+    public class CarouselMenu : ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+        public CarouselMenu(ApplicationDbContext context)
+        {
+            db = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var clothes = await db.Clothes.Where(x => x.IsPopularItem).ToListAsync();
+            return View(clothes);
+        }
+    }
+}
