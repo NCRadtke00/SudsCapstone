08b2d8c8392888c58837a03ef8eebd5f573ece6a
Nicholas Radtke <Raddnc@gmail.com>
2020-11-16 19:15:18 -0600

    added services model

---
+++ Sud/Models/Services.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sud.Models
+{
+    public class Services
+    {
+        public Services()
+        {
+            Clothes = new HashSet<Clothes>();
+        }
+
+        public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 2)]
+        [RegularExpression("([a-zA-Z0-9 .&'-]+)", ErrorMessage = "The field Name should only include letters and number.")]
+        [DataType(DataType.Text)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(255, MinimumLength = 2)]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public virtual ICollection<Clothes> Clothes { get; set; }
+    }
+}
