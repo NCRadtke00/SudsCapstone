7abf15579af4c84749223d4f49efac9372bc0e71
Nicholas Radtke <Raddnc@gmail.com>
2020-11-16 20:52:19 -0600

    created service interface

---
+++ Sud/Repositories/IServicesRepository.cs
@@ -0,0 +1,23 @@
+﻿using Sud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sud.Repositories
+{
+    interface IServicesRepository
+    {
+        IEnumerable<Services> Services { get; }
+        Services GetById(int? id);
+        Task<Services> GetByIdAsync(int? id);
+        bool Exists(int id);
+        IEnumerable<Services> GetAll();
+        Task<IEnumerable<Services>> GetAllAsync();
+        void Add(Services services);
+        void Update(Services services);
+        void Remove(Services services);
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
