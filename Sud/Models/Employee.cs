12df7bea2c45ccdca308991975a4077abcf41bf8
Nicholas Radtke <Raddnc@gmail.com>
2020-11-16 20:27:28 -0600

    created employee model

---
+++ Sud/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Sud.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
