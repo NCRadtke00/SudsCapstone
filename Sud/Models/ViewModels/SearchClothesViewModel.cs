aec60dd50a040ef64a2c6f71173f105d4d509d47
Nicholas Radtke <Raddnc@gmail.com>
2020-11-16 19:58:32 -0600

    added SearchClothesViewModel

---
+++ Sud/Models/ViewModels/SearchClothesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sud.Models.ViewModels
+{
+    public class SearchClothesViewModel
+    {
+        [Required]
+        [DisplayName("Serach")]
+        public string SearchText { get; set; }
+        public IEnumerable<Clothes> ClothesList { get; set; }
+    }
+}
