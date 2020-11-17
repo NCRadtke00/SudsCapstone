61b18a12eba5206eca30fdd59eec5948bfa4d6ed
Nicholas Radtke <Raddnc@gmail.com>
2020-11-16 21:25:01 -0600

    working on OrderRepository

---
+++ Sud/Repositories/OrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Sud.Data;
+using Sud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sud.Repositories
+{
+    public class OrderRepository
+    {
+        private readonly ApplicationDbContext db;
+        private readonly ShoppingCart _shoppingCart;
+        public OrderRepository(ApplicationDbContext context, ShoppingCart shoppingCart)
+        {
+            db = context;
+            _shoppingCart = shoppingCart;
+        }
+    }
+}
