339f8e62a75aa058ecc2851d3c71c4dc137d574f
Nicholas Radtke <Raddnc@gmail.com>
2020-11-16 20:26:41 -0600

    added customer model

---
+++ Sud/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sud.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
