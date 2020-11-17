799d65fa22a830d45522c9ad17a642519d6330cb
Nicholas Radtke <Raddnc@gmail.com>
2020-11-16 20:49:20 -0600

    created clotherepo and interface

---
+++ Sud/Repositories/ClotheRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sud.Data;
+using Sud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sud.Repositories
+{
+    public class ClotheRepository : IClotheRepository
+    {
+        private readonly ApplicationDbContext db;
+        public ClotheRepository(ApplicationDbContext context)
+        {
+            db = context;
+        }
+        public IEnumerable<Clothes> Clothes => db.Clothes.Include(p => p.Services).Include(p => p.Reviews); //include here
+        public IEnumerable<Clothes> PopularItem => db.Clothes.Where(p => p.IsPopularItem).Include(p => p.Services);
+        public void Add(Clothes clothes)
+        {
+            db.Add(clothes);
+        }
+        public IEnumerable<Clothes> GetAll()
+        {
+            return db.Clothes.ToList();
+        }
+        public async Task<IEnumerable<Clothes>> GetAllAsync()
+        {
+            return await db.Clothes.ToListAsync();
+        }
+        public async Task<IEnumerable<Clothes>> GetAllIncludedAsync()
+        {
+            return await db.Clothes.Include(p => p.Services).Include(p => p.Reviews).ToListAsync();
+        }
+        public IEnumerable<Clothes> GetAllIncluded()
+        {
+            return db.Clothes.Include(p => p.Services).Include(p => p.Reviews).ToList();
+        }
+        public Clothes GetById(int? id)
+        {
+            return db.Clothes.FirstOrDefault(p => p.Id == id);
+        }
+        public async Task<Clothes> GetByIdAsync(int? id)
+        {
+            return await db.Clothes.FirstOrDefaultAsync(p => p.Id == id);
+        }
+        public Clothes GetByIdIncluded(int? id)
+        {
+            return db.Clothes.Include(p => p.Services).Include(p => p.Reviews).FirstOrDefault(p => p.Id == id);
+        }
+        public async Task<Clothes> GetByIdIncludedAsync(int? id)
+        {
+            return await db.Clothes.Include(p => p.Services).Include(p => p.Reviews).FirstOrDefaultAsync(p => p.Id == id);
+        }
+        public bool Exists(int id)
+        {
+            return db.Clothes.Any(p => p.Id == id);
+        }
+        public void Remove(Clothes clothes)
+        {
+            db.Remove(clothes);
+        }
+        public async Task SaveChangesAsync()
+        {
+            await db.SaveChangesAsync();
+        }
+        public void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+        public void Update(Clothes clothes)
+        {
+            db.Update(clothes);
+        }
+    }
+}
---
+++ Sud/Repositories/IClotheRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sud.Models;
+
+namespace Sud.Repositories
+{
+    interface IClotheRepository
+    {
+        IEnumerable<Clothes> Clothes { get; }
+        IEnumerable<Clothes> PopularItem { get; }
+        Clothes GetById(int? id);
+        Task<Clothes> GetByIdAsync(int? id);
+        Clothes GetByIdIncluded(int? id);
+        Task<Clothes> GetByIdIncludedAsync(int? id);
+        bool Exists(int id);
+        IEnumerable<Clothes> GetAll();
+        Task<IEnumerable<Clothes>> GetAllAsync();
+        IEnumerable<Clothes> GetAllIncluded();
+        Task<IEnumerable<Clothes>> GetAllIncludedAsync();
+        void Add(Clothes clothes);
+        void Update(Clothes clothes);
+        void Remove(Clothes clothes);
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
