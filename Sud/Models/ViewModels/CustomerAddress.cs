810b6425d121d1157738bee84887e6fda392dda2
Nicholas Radtke <Raddnc@gmail.com>
2020-11-21 19:48:54 -0600

    added CustomerAddress view model

---
+++ Sud/Models/ViewModels/CustomerAddress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Sud.Models.ViewModels
+{
+    public class CustomerAddress
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public int ZipCode { get; set; }
+
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
