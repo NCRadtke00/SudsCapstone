3893d827ca1e471a2f7a2a3b8aa53907006d7175
Nicholas Radtke <Raddnc@gmail.com>
2020-11-21 19:48:42 -0600

    added CustomersByPickUpAndDropOffDay viewmodel

---
+++ Sud/Models/ViewModels/CustomersByPickUpAndDropOffDay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Sud.Models.ViewModels
+{
+    public class CustomersByPickUpAndDropOffDay
+    {
+        public SelectList DaySelection { get; set; }
+        [Display(Name = "Which day would you like to see?")]
+        public string DaySelected { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
+    }
+}
