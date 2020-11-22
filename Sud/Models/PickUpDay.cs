289c2d886e716edcddad51e423a36c6e7dbb3be3
Nicholas Radtke <Raddnc@gmail.com>
2020-11-21 19:46:46 -0600

    added pickupday model

---
+++ Sud/Models/PickUpDay.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sud.Models
+{
+    public class PickUpDay
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Pickup Day")]
+        public string Date { get; set; }
+    }
+}
