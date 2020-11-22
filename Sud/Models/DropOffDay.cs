c6c9bdb52bbc5d1d0df07f0a4124f0befd9438b6
Nicholas Radtke <Raddnc@gmail.com>
2020-11-21 19:46:36 -0600

    added drop off day model

---
+++ Sud/Models/DropOffDay.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sud.Models
+{
+    public class DropOffDay
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Drop off Day")]
+        public string Date { get; set; }
+    }
+}
