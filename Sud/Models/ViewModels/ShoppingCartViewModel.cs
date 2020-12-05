00d2397bd4f16015c2bafbca8efa8086f55b72d4
Nicholas Radtke <Raddnc@gmail.com>
2020-12-05 00:41:44 -0600

    changed dec to dbl

--- Sud/Models/ViewModels/ShoppingCartViewModel.cs
+++ Sud/Models/ViewModels/ShoppingCartViewModel.cs
@@ -8,6 +8,6 @@ namespace Sud.Models.ViewModels
     public class ShoppingCartViewModel
     {
         public ShoppingCart ShoppingCart { get; set; }
-        public decimal ShoppingCartTotal { get; set; }
+        public double ShoppingCartTotal { get; set; }
     }
 }
