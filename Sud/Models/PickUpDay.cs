14b9e316a8e4337c6c6651c3dc187a80b49f2c2d
Nicholas Radtke <Raddnc@gmail.com>
2020-12-05 00:42:48 -0600

    fixed how pickups are set so that its done in the order checkout vs when account is created

--- Sud/Models/PickUpDay.cs
+++ Sud/Models/PickUpDay.cs
@@ -1,12 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Sud.Models
 {
     public class PickUpDay
     {
-        [Key]
-        public int Id { get; set; }
-        [Display(Name = "Pickup Day")]
-        public string Date { get; set; }
+        public static List<SelectListItem> PickupDay = new List<SelectListItem>()
+        {
+            new SelectListItem() { Text="Monday", Value="Monday" },
+            new SelectListItem() { Text="Tuesday", Value="Tuesday" },
+            new SelectListItem() { Text="Wednesday", Value="Wednesday" },
+            new SelectListItem() { Text="Thursday", Value="Thursday" },
+            new SelectListItem() { Text="Friday", Value="Friday" },
+            new SelectListItem() { Text="Saturday", Value="Saturday" },
+            new SelectListItem() { Text="Sunday", Value="Sunday" },
+        };
     }
 }
