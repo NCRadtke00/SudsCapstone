3d9209e0ededa9d4c35911fef35a6aa8fd9cca6e
Nicholas Radtke <Raddnc@gmail.com>
2020-12-05 00:46:27 -0600

    changed dropoff day class so it can be set in order vs profile edits

--- Sud/Models/DropOffDay.cs
+++ Sud/Models/DropOffDay.cs
@@ -1,12 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
 
 namespace Sud.Models
 {
     public class DropOffDay
     {
-        [Key]
-        public int Id { get; set; }
-        [Display(Name = "Drop off Day")]
-        public string Date { get; set; }
+        public static List<SelectListItem> DropoffDay = new List<SelectListItem>()
+        {
+            new SelectListItem() { Text="Monday", Value="Monday" },
+            new SelectListItem() { Text="Tuesday", Value="Tuesday" },
+            new SelectListItem() { Text="Wednesday", Value="Wednesday" },
+            new SelectListItem() { Text="Thursday", Value="Thursday" },
+            new SelectListItem() { Text="Friday", Value="Friday" },
+            new SelectListItem() { Text="Saturday", Value="Saturday" },
+            new SelectListItem() { Text="Sunday", Value="Sunday" },
+        };
     }
 }
