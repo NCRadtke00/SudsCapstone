124a3c769b5159385ff32ef6facb90cd975abe7e
Nicholas Radtke <Raddnc@gmail.com>
2020-12-08 13:56:06 -0600

    added stripe info

--- Sud/wwwroot/js/site.js
+++ Sud/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+<script src="https://js.stripe.com/v3/"></script>
+
+var stripe = Stripe('pk_test_51Hn9DtFfxvFC7Iq0iyfNbG2W1WpvsrknMUcCIzwJ2DgWYEV1vMQInmx4inXyacbqE6WsXzScNYLZE2JUbWks4nwQ00owATw32J');
