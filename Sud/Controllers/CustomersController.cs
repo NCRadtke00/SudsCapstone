a3572f374d653467d2bd1fcb3292958f8c5c96da
Nicholas Radtke <Raddnc@gmail.com>
2020-12-07 21:36:25 -0600

    fixed geocoding issues when customers create account.
    . and edits a method

--- Sud/Controllers/CustomersController.cs
+++ Sud/Controllers/CustomersController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -53,68 +53,156 @@ namespace Sud.Controllers
         }
         public IActionResult Create() //get
         {
-            var days = _context.PickUpDays.ToList();
-            var dates = _context.DropOffDays.ToList();
-            
-            Customer customer = new Customer()
-            {
-                Days = new SelectList(days, "Id", "Date"),
-                Dates = new SelectList(dates, "Id", "Date")
-            };
-            return View(customer);
+
+            return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Customer customer) // post
+        public async Task<IActionResult> Create(Customer customer) // post
         {
+            //the line of code below this isnt working with redirect to api keys
+            //string url = $"https://maps.googleapis.com/maps/api/geocode/json?address={customer.Address.StreetAddress},+{customer.Address.City},+{customer.Address.State},{customer.Address.ZipCode}&key={Sud.APIKeys.Key}";
+            
+            string url = $"https://maps.googleapis.com/maps/api/geocode/json?address={customer.Address.StreetAddress},+{customer.Address.City},+{customer.Address.State},{customer.Address.ZipCode}&key=AIzaSyDXS87aNNUzLOl40Q1kuMBWqup20n-508M";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            string jsonResult = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                JObject geoCode = JObject.Parse(jsonResult);
+                customer.Address.Latitude = (double)geoCode["results"][0]["geometry"]["location"]["lat"];
+
+                customer.Address.Longitude = (double)geoCode["results"][0]["geometry"]["location"]["lng"];
+            }
             if (ModelState.IsValid)
             {
                 var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
                 customer.IdentityUserId = userId;
-                customer.PickUpDay = _context.PickUpDays.Find(customer.PickUpDayId);
-                customer.DropOffDay = _context.DropOffDays.Find(customer.DropOffDayId);
-                _context.Add(customer);
-                _context.SaveChanges();
+                _db.Add(customer);
+                await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            else
-            {
-                return View(customer);
-            }
-
+            // ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", customer.IdentityUserId);
+            return View(customer);
         }
-        public IActionResult Edit(int? id)
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create(Customer customer)
+        //{
+        //    var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        //    customer.IdentityUserId = userId;
+        //    customer.Address = await GeocodeAddress(customer.Address);
+        //    _db.Add(customer);
+        //    await _db.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        //private async Task<Address> GeocodeAddress(Address address)
+        //{
+        //    string formattedAddress = address.ToString();
+        //    Uri fullURL = new Uri("https://maps.googleapis.com/maps/api/geocode/json?address=" + formattedAddress + "&key=" + APIKeys.GoogleMapsKey);
+        //    var response = await httpClient.GetAsync(fullURL);
+
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var task = response.Content.ReadAsStringAsync().Result;
+        //        JObject mapsData = JsonConvert.DeserializeObject<JObject>(task);
+        //        address.Latitude = Convert.ToDouble(mapsData["results"][0]["geometry"]["location"]["lat"]);
+        //        address.Longitude = Convert.ToDouble(mapsData["results"][0]["geometry"]["location"]["lng"]);
+        //    }
+
+        //    return address;
+        //}
+
+        //private string ParseAddress(Address address)
+        //{
+        //    StringBuilder stringBuilder = new StringBuilder();
+        //    for (int i = 0; i < address.StreetAddress; i++)
+        //    {
+        //        if (address.StreetAddress[i] == ' ')
+        //        {
+        //            stringBuilder.Append("+");
+        //        }
+        //        else
+        //        {
+        //            stringBuilder.Append(address.StreetAddress[i]);
+        //        }
+        //    }
+        //    stringBuilder.Append(",+");
+        //    for (int i = 0; i < address.City.Length; i++)
+        //    {
+        //        if (address.City[i] == ' ')
+        //        {
+        //            stringBuilder.Append("+");
+        //        }
+        //        else
+        //        {
+        //            stringBuilder.Append(address.City[i]);
+        //        }
+        //    }
+        //    stringBuilder.Append(",+");
+        //    for (int i = 0; i < address.State.Length; i++)
+        //    {
+        //        if (address.State[i] == ' ')
+        //        {
+        //            stringBuilder.Append("+");
+        //        }
+        //        else
+        //        {
+        //            stringBuilder.Append(address.State[i]);
+        //        }
+        //    }
+        //    return stringBuilder.ToString();
+        //}
+        public async Task<IActionResult> Edit(int? id)
         {
-            var customer = _context.Customer.SingleOrDefault(c => c.Id == id);
-            var days = _context.PickUpDays.ToList();
-            customer.Days = new SelectList(days, "Id", "Date");
-            var dates = _context.DropOffDays.ToList();
-            customer.Dates = new SelectList(dates, "Id", "Date");
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var customer = await _db.Customer.FindAsync(id);
             if (customer == null)
             {
                 return NotFound();
             }
+            ViewData["IdentityUserId"] = new SelectList(_db.Users, "Id", "Id", customer.IdentityUserId);
             return View(customer);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Customer customer)
+        public async Task<IActionResult> Edit(int id, Customer customer)
         {
-            try
+            if (id != customer.Id)
             {
-                _context.Customer.Update(customer);
-                _context.SaveChanges();
-                return RedirectToAction("Details", customer);
+                return NotFound();
             }
-            catch
+            if (ModelState.IsValid)
             {
-                return RedirectToAction("Index");
+                try
+                {
+                    _db.Update(customer);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
             }
+            ViewData["IdentityUserId"] = new SelectList(_db.Users, "Id", "Id", customer.IdentityUserId);
+            return View(customer);
         }
 
         private bool CustomerExists(int id)
         {
-            return _context.Customer.Any(e => e.Id == id);
+            return _db.Customer.Any(e => e.Id == id);
         }
     }
 }
