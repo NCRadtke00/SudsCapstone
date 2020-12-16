d4d1f92a57409fd425b570be200eda9ac7f0dc20
Nicholas Radtke <Raddnc@gmail.com>
2020-12-16 14:36:44 -0600

    created imagemodelscontroller, made delete method remove from the database and create method save to database

---
+++ Sud/Controllers/ImageModelsController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sud.Data;
+using Sud.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Sud.Controllers
+{
+    public class ImageModelsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ImageModelsController(ApplicationDbContext _db, IWebHostEnvironment hostEnvironment)
+        {
+            db = _db;
+            this._hostEnvironment = hostEnvironment;
+        }
+
+        // GET: ImageModels
+        public async Task<IActionResult> Index()
+        {
+            await db.Images.ToListAsync();
+            return View();
+        }
+
+        // GET: ImageModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imageModel = await db.Images
+                .FirstOrDefaultAsync(m => m.ImageId == id);
+            if (imageModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(imageModel);
+        }
+
+        // GET: ImageModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ImageModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ImageId,Title,ImageName")] ImageModel imageModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+                string extention = Path.GetExtension(imageModel.ImageFile.FileName);
+                fileName = fileName + DateTime.Now.ToString("yymmssfff") + extention;
+                string path = Path.Combine(wwwRootPath + "/Image" + fileName);
+                using (var fileStream = new FileStream(path, FileMode.Create))
+                {
+                    await imageModel.ImageFile.CopyToAsync(fileStream);
+                }
+
+                db.Add(imageModel);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(imageModel);
+        }
+
+        // GET: ImageModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imageModel = await db.Images.FindAsync(id);
+            if (imageModel == null)
+            {
+                return NotFound();
+            }
+            return View(imageModel);
+        }
+
+        // POST: ImageModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ImageId,Title,ImageName")] ImageModel imageModel)
+        {
+            if (id != imageModel.ImageId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Update(imageModel);
+                    await db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ImageModelExists(imageModel.ImageId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(imageModel);
+        }
+
+        // GET: ImageModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imageModel = await db.Images
+                .FirstOrDefaultAsync(m => m.ImageId == id);
+            if (imageModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(imageModel);
+        }
+
+        // POST: ImageModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var imageModel = await db.Images.FindAsync(id);
+
+            var imagePath = Path.Combine(_hostEnvironment.WebRootPath, "image", imageModel.ImageName);
+            if (System.IO.File.Exists(imagePath))
+                System.IO.File.Delete(imagePath);
+
+            db.Images.Remove(imageModel);
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ImageModelExists(int id)
+        {
+            return db.Images.Any(e => e.ImageId == id);
+        }
+    }
+}
