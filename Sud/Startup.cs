ddbda0ab175281512f7f70be748e31d75537a6ab
Nicholas Radtke <Raddnc@gmail.com>
2021-02-18 01:04:19 -0600

    fixed account login issue

--- Sud/Startup.cs
+++ Sud/Startup.cs
@@ -39,7 +39,7 @@ namespace Sud
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
